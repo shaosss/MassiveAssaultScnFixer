e9447b98e3df04175a1caebdf65798bfe350af6f
Alexandr Shevchenko <shao-sss@hotmail.com>
2021-04-05 12:02:53 +0300

    Implement fixer

---
+++ FileFixer.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MassiveAssaultScnFixer
+{
+    public class FileFixer
+    {
+        private readonly string _scnFilePath;
+        private readonly string _mscFilePath;
+
+        private bool _lastScnLineWasEmpty = false;
+        private readonly Dictionary<int, int> _unitIdMapping = new Dictionary<int, int>();
+        private int _unitCounter = 1;
+        
+        public FileFixer(string file)
+        {
+            var directory = Path.GetDirectoryName(file) 
+                         ?? throw new ArgumentException("File is not exists", nameof(file));
+            _scnFilePath = file;
+            _mscFilePath = Path.Combine(directory, $"{Path.GetFileNameWithoutExtension(file)}.msc");
+        }
+
+        public void Fix()
+        {
+            if (!File.Exists(_mscFilePath))
+            {
+                Console.WriteLine($"{_scnFilePath} is a NET scenario. Skipped.");
+                return;
+            }
+            Console.WriteLine($"Fixing: {_scnFilePath}");
+            FixFile(_scnFilePath, ProcessScnLine);
+            Console.WriteLine($"Fixing: {_mscFilePath}");
+            FixFile(_mscFilePath, ProcessMscLine);
+        }
+
+        private void FixFile(string originalPath, Func<string?, string?> processingFunc)
+        {
+            var newScnFilePath = $"{originalPath}.new";
+            using (var newScnFile = File.CreateText(newScnFilePath))
+            using (var originalFile = File.OpenText(originalPath))
+            {
+                for (string? originalLine = originalFile.ReadLine(); originalLine != null; 
+                    originalLine = originalFile.ReadLine())
+                {
+                    var fixedLine = processingFunc(originalLine);
+                    if (fixedLine != null)
+                        newScnFile.WriteLine(fixedLine);
+                }
+            }
+            File.Replace(newScnFilePath, originalPath, null);
+        }
+
+        private string? ProcessScnLine(string? originalLine)
+        {
+            if (originalLine == null 
+                || string.IsNullOrEmpty(originalLine) && _lastScnLineWasEmpty 
+                || originalLine.StartsWith(';'))
+                return null;
+
+            if (originalLine == string.Empty)
+            {
+                _lastScnLineWasEmpty = true;
+                return string.Empty;
+            }
+
+            _lastScnLineWasEmpty = false;
+
+            var tokens = originalLine.Split(',');
+            if (tokens[0].EndsWith("UNIT"))
+            {
+                var originalUnitId = int.Parse(tokens[1]);
+                _unitIdMapping[originalUnitId] = _unitCounter;
+                tokens[1] = _unitCounter.ToString();
+                ++_unitCounter;
+                return string.Join(',', tokens);
+            }
+
+            return originalLine;
+        }
+        
+        private string? ProcessMscLine(string? originalLine)
+        {
+            if (originalLine != null && (originalLine.StartsWith("SELECT_UNIT2") || originalLine.StartsWith("UNSELECT_UNIT2")))
+            {
+                var tokens = originalLine.Split(' ');
+                var unitId = int.Parse(tokens[1]);
+                
+                //fix for convoy scenario (there is typo in the Id)
+                if (unitId == 41050)
+                    unitId = 4105;
+                if (!_unitIdMapping.ContainsKey(unitId))
+                {
+                    Console.WriteLine($"Unit with id {unitId} is not mapped!");
+                    return null;
+                }
+
+                tokens[1] = _unitIdMapping[unitId].ToString();
+                return string.Join(' ', tokens);
+            }
+
+            return originalLine;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace MassiveAssaultScnFixer
+{
+    class Program
+    {
+        private const string ScenariosFolderName = "scenarios";
+        private const string ScenariosBackupFolderName = "scenarios_backup";
+        
+        static void Main(string[] args)
+        {
+            var gameFolder = args[0];
+            FixFilesInFolder(gameFolder);
+        }
+
+        private static void FixFilesInFolder(string gameFolder)
+        {
+            var scenariosFolder = Path.Combine(gameFolder, ScenariosFolderName);
+            BackupScenarios(gameFolder, scenariosFolder);
+            var files = Directory.GetFiles(scenariosFolder, "*.scn", SearchOption.AllDirectories);
+            foreach (var file in files)
+            {
+                try
+                {
+                    var fixer = new FileFixer(file);
+                    fixer.Fix();
+                    Console.WriteLine($"Scenario is fixed: {file}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Failed on fixing scenario: {file}");
+                    Console.WriteLine(e);
+                }
+            }
+        }
+
+        private static void BackupScenarios(string gameFolder, string scenariosFolder)
+        {
+            var backupFolder = Path.Combine(gameFolder, ScenariosBackupFolderName);
+            foreach (string dirPath in Directory.GetDirectories(scenariosFolder, "*", 
+                SearchOption.AllDirectories))
+                Directory.CreateDirectory(dirPath.Replace(scenariosFolder, backupFolder));
+            
+            foreach (string newPath in Directory.GetFiles(scenariosFolder, "*.*", 
+                SearchOption.AllDirectories))
+                File.Copy(newPath, newPath.Replace(scenariosFolder, backupFolder), true);
+        }
+    }
+}
